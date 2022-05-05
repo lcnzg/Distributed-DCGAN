bc272e1e63821d3bd0969ef1b6dd24e8591736b7
Luciano Zago <lcnzg7@gmail.com>
2022-05-05 13:59:39 -0300

    Reduce batch size to 16, due to insufficient shared memory

--- experiments/ativ-5-exp-1/run.sh
+++ experiments/ativ-5-exp-1/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BATCH_SIZE=32
+BATCH_SIZE=16
 PROCS=(1 2 4 8)
 EPOCHS=1
 REPEAT=3
@@ -15,7 +15,7 @@ for PROC in "${PROCS[@]}"
 do
   for ((i=0; i < REPEAT; i++))
   do
-    echo "CIFAR-10 DCGAN batch:$BATCH procs:$PROC epochs:$EPOCHS run:$i"
+    echo "CIFAR-10 DCGAN batch:$BATCH_SIZE procs:$PROC epochs:$EPOCHS run:$i"
     time -p docker run --user=$(id -u):$(id -g) --env OMP_NUM_THREADS=1 --rm --network=host -v=$(pwd):/root dist_dcgan:latest python -m torch.distributed.launch --nproc_per_node=$PROC --nnodes=1 --node_rank=0 --master_addr="172.17.0.1" --master_port=1234 dist_dcgan.py --dataset cifar10 --dataroot ./cifar10 --batch_size=$BATCH_SIZE --num_epochs=$EPOCHS
   done
 done
